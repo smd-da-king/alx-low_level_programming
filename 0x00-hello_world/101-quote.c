657f84ee9404d9787b51c46e5537d288439d819e
smd <toyin_ng@yahoo.com>
2023-02-16 14:08:02 -0800

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 /**
@@ -13,19 +12,3 @@ int main(void)
 
 	return (1);
 }
-=======
-#include <stdio.h>
-
-/**
-  * main - Prints a string to the standard output
-  *
-  *Return: 1 (Success)
-  */
-
-int main(void)
-{
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	return (1);
-}
->>>>>>> f113072ca402a558d6ef05ec00f80c211760b2c9
