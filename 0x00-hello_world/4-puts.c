e9c0025cc75136acc0ad93b7f5e643697169873d
smd <toyin_ng@yahoo.com>
2023-02-16 13:36:42 -0800

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
- #include <stdio.h>
- /**
- *main - Prints string to the standard output
- *
- * Return: 0 (Success)
- */
-
-int main(void)
-{
-char Mystr[] = "\"Programming is like building a multilingual puzzle";
-
-puts(Mystr);
-	
-return (0);
-=======
 #include <stdio.h>
 
 /**
@@ -29,5 +13,4 @@ int main(void)
 	puts(str);
 
 	return (0);
->>>>>>> 4dcaad3ca746abf2de21f2b58cd1d7ed90b132a5
 }
