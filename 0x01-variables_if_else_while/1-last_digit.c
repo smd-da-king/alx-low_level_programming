bcf0cc3ee817af9913687e7356efe7e1c3f831a5
smd <toyin_ng@yahoo.com>
2023-02-17 06:28:54 -0800

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,13 +26,9 @@ int main(void)
 		{
 			printf("%d and is 0\n", m);
 		}
-		else if (m > 6 && m < 0)
-		{
-			printf("%d and is less than 6 and not 0\n", m);
-		}
 		else
 		{
-			printf("end");
+			printf("%d and is less than 6 and not 0\n", m);
 		}
 	/* your code goes there */
 	return (0);
