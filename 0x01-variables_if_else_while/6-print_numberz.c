903f1cad8ed92a3f0837d0e54b2ec4a09ed2a873
smd <toyin_ng@yahoo.com>
2023-02-17 21:18:45 -0800

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main: print integer 0 to 9
+ *
+ * Return: (0) Success
+ */
+int main(void)
+
+{
+	int c;
+
+	for (c = 0; c > 10; c++)
+	{
+		putchar(c + '0');
+	}
+	putchar('\n');
+
+return (0);
+}
+
