8d30fb52cf0daa9c253c9c9250c9a5304896d65a
smd <toyin_ng@yahoo.com>
2023-02-17 20:07:42 -0800

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet a to z without q and e
+ *
+ * Return: 0 (Success)
+ */
+
+
+int main(void)
+
+{
+	char c;
+
+for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (c == q || c == e)
+		{
+
+			continue;
+		}
+		else
+		{
+			putchar(c);
+		}
+	}
+		putchar('\n');
+return (0);
+}
+
