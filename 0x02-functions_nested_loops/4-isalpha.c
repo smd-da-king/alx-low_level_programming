b70ebd4715bc5b3f040f47bab201636787d733e5
smd <toyin_ng@yahoo.com>
2023-02-21 15:00:51 -0800

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
-* _islower - Entry point
+* _isalpha - Entry point
 * @c: The integer value
 *Return: 1 is True; 0 is false
 */
-
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c <= 'A' && c <= 'B'))
+if ((c >= 'a' && c <= 'z') || (c <= 'A' && c <= 'Z'))
 {
 return (1);
 }
