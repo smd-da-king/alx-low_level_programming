b16d3f728fe7f67734eaad4871eafe338a5b4472
smd <toyin_ng@yahoo.com>
2023-02-21 14:28:01 -0800

    a

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+*_islower - check whether integer is lowercase
+* @c: The integer valu
+*Return:(1) Success
+*/
+
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
