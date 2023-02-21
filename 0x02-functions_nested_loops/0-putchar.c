db05b81d9e0a62dd2a4c63218b72b6986eab956c
smd <toyin_ng@yahoo.com>
2023-02-21 02:02:05 -0800

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - printing putchar
+*
+* Return:( 0) Success
+*/
+
+int main(void)
+{
+int a;
+
+a = '_';
+putchar(a);
+a = 'p';
+putchar(a);
+a = 'u';
+putchar(a);
+a = 't';
+putchar(a);
+a = 'c';
+putchar(a);
+a = 'h';
+putchar(a);
+a = 'a';
+putchar(a);
+a = 'r';
+putchar(a);
+putchar('\n');
+return (0);
+}
