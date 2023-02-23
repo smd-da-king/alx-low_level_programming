74e502aaa491a7450bd9c4d5a585ec28de4d6bcf
smd-da-king <107342453+smd-da-king@users.noreply.github.com>
2023-02-23 06:51:01 +0100

    a

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include "main.h"
+include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - Entry point
+ * Description - Test function that prints if integer is positive ornegative
+ * Return: 0
+ */
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
-        positive_or_negative(i);
-
-        return (0);
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
