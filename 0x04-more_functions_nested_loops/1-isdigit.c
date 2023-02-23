a5c3bb6051114d29ff220b75a2be925b11810bb3
smd <toyin_ng@yahoo.com>
2023-02-23 09:43:37 -0800

    a

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _isdigit - checking digits
+* @c: variable declaration'
+*
+* Return: 0
+*/
+
+int _isdigit(int c)
+{
+if (c >= 0 &&  c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
