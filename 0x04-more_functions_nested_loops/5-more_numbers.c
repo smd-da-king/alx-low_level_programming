db3f896abe339361e7e33fff70f31cbc55464eb4
smd <toyin_ng@yahoo.com>
2023-02-24 00:31:50 -0800

    a

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*more_numbers - printing more number
+* i: integer i
+* j: integer j
+*/
+
+void more_numbers(void)
+{
+int i, j;
+
+for (i = 0; i<=10; i++)
+{
+for( j = 0; j <= 14; j++)
+{
+if (j >= 10)
+{
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+}
+else
+{
+_putchar('0' + j);
+}
+}
+_putchar('\n');
+}
+}
