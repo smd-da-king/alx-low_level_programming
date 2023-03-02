037caba85661c7d15d88c769b3780f6b7b0bcbb4
smd <toyin_ng@yahoo.com>
2023-03-01 21:09:20 -0800

    a

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - comparing strings
+ * @s1: var1
+ * @s2: var2
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int output;
+
+output = strcmp(s1, s2);
+return (output);
+}
