2de97035a274042fa303a2549ac0ca89ab901977
smd <toyin_ng@yahoo.com>
2023-03-08 11:34:50 -0800

    a

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* is_palindrome - function name
+* @i: var1
+* @length: var2
+* @flag: var3
+* Return: 0- fail and 1- success
+*/
+
+int is_palindrome(char *s)
+{
+int i, length;
+int flag = 0;
+length = strlen(s);    
+for(i = 0; i < length; i++)
+{
+if (s[i] != s[length-i-1])
+{
+flag = 1;
+break;
+}
+}
+if (flag)
+{
+return 0;
+}    
+else
+{
+return 1;
+}
+  
+}
